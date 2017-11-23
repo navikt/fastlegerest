cee628bc5370ad08d87b7d44eab046641a8f604a
Andreas Lund <andreas.lund@nav.no>
2017-11-23 09:05:42 +0100

    Fikser konflikt.

--- provider/src/main/java/no/nav/syfo/rest/ressurser/FastlegeRessurs.java
+++ provider/src/main/java/no/nav/syfo/rest/ressurser/FastlegeRessurs.java
@@ -1,28 +1,18 @@
 package no.nav.syfo.rest.ressurser;
 
-import com.google.gson.Gson;
 import io.swagger.annotations.Api;
 import no.nav.metrics.aspects.Count;
 import no.nav.metrics.aspects.Timed;
 import no.nav.syfo.domain.*;
 import no.nav.syfo.services.FastlegeService;
-import no.nav.syfo.services.LdapService;
 import no.nav.syfo.services.TilgangService;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
-import java.util.List;
-
-import static java.lang.System.*;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.ok;
-import static no.nav.brukerdialog.security.context.SubjectHandler.getSubjectHandler;
-import static no.nav.metrics.MetricsFactory.createEvent;
-import static org.springframework.util.StringUtils.*;
 
 @Path("/fastlege/v1")
 @Consumes(APPLICATION_JSON)
@@ -66,7 +56,7 @@ public class FastlegeRessurs {
                     .type(APPLICATION_JSON)
                     .build();
         }
-        return Response.ok(fastlegeService.hentBrukersFastleger(fnr));
+        return Response.ok(fastlegeService.hentBrukersFastleger(fnr)).build();
     }
 
     @GET
