246f6adb8893c981fbcc438ba252cd874abbd85c
Engelien, Stefan Andreas <stefan.andreas.engelien@nav.no>
2018-01-23 10:27:25 +0100

    SYFOUTV-1804 Ta med brukers sso-token i kallet mot syfo-tilgangskontroll

--- provider/src/main/java/no/nav/syfo/services/TilgangService.java
+++ provider/src/main/java/no/nav/syfo/services/TilgangService.java
@@ -1,5 +1,6 @@
 package no.nav.syfo.services;
 
+import no.nav.brukerdialog.security.context.SubjectHandler;
 import org.springframework.cache.annotation.Cacheable;
 
 import javax.ws.rs.core.MediaType;
@@ -7,21 +8,26 @@ import javax.ws.rs.core.Response;
 
 import static java.lang.System.getProperty;
 import static javax.ws.rs.client.ClientBuilder.newClient;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 
 public class TilgangService {
 
-//    @Cacheable(value = "tilgang", keyGenerator = "userkeygenerator")
+    @Cacheable(value = "tilgang", keyGenerator = "userkeygenerator")
     public Response sjekkTilgang(String fnr) {
+        String ssoToken = SubjectHandler.getSubjectHandler().getInternSsoToken();
         return newClient().target(getProperty("syfo-tilgangskontroll-api.url") + "/tilgangtilbruker")
                 .queryParam("fnr", fnr)
                 .request(MediaType.APPLICATION_JSON)
+                .header(AUTHORIZATION, "Bearer " + ssoToken)
                 .get();
     }
 
-//    @Cacheable(value = "tilgang", keyGenerator = "userkeygenerator")
+    @Cacheable(value = "tilgang", keyGenerator = "userkeygenerator")
     public Response harTilgangTilTjenesten() {
+        String ssoToken = SubjectHandler.getSubjectHandler().getInternSsoToken();
         return newClient().target(getProperty("syfo-tilgangskontroll-api.url") + "/tilgangtiltjenesten")
                 .request(MediaType.APPLICATION_JSON)
+                .header(AUTHORIZATION, "Bearer " + ssoToken)
                 .get();
     }
 
