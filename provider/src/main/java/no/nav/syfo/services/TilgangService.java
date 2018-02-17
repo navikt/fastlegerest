c8041c3a32bbdb03a5c8d9171aa6df849b6721ec
Andreas Lund <andreas.lund@nav.no>
2018-02-17 07:08:35 +0100

    Gjenbruker jerseyklienten istedenfor å newe opp den hver gang.

--- provider/src/main/java/no/nav/syfo/services/TilgangService.java
+++ provider/src/main/java/no/nav/syfo/services/TilgangService.java
@@ -3,6 +3,7 @@ package no.nav.syfo.services;
 import no.nav.brukerdialog.security.context.SubjectHandler;
 import org.springframework.cache.annotation.Cacheable;
 
+import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -12,10 +13,12 @@ import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 
 public class TilgangService {
 
+    private Client client = newClient();
+
     @Cacheable(value = "tilgang", keyGenerator = "userkeygenerator")
     public Response sjekkTilgang(String fnr) {
         String ssoToken = SubjectHandler.getSubjectHandler().getInternSsoToken();
-        return newClient().target(getProperty("syfo-tilgangskontroll-api.url") + "/tilgangtilbruker")
+        return client.target(getProperty("syfo-tilgangskontroll-api.url") + "/tilgangtilbruker")
                 .queryParam("fnr", fnr)
                 .request(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, "Bearer " + ssoToken)
@@ -25,7 +28,7 @@ public class TilgangService {
     @Cacheable(value = "tilgang", keyGenerator = "userkeygenerator")
     public Response harTilgangTilTjenesten() {
         String ssoToken = SubjectHandler.getSubjectHandler().getInternSsoToken();
-        return newClient().target(getProperty("syfo-tilgangskontroll-api.url") + "/tilgangtiltjenesten")
+        return client.target(getProperty("syfo-tilgangskontroll-api.url") + "/tilgangtiltjenesten")
                 .request(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, "Bearer " + ssoToken)
                 .get();
