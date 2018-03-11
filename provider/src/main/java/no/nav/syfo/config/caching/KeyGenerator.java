3b14820544e35b1d6c92ba4ff68844a752cd7746
Andreas Lund <andreas.lund@nav.no>
2018-03-11 10:09:13 +0100

    Setter riktig properties lokalt

--- provider/src/main/java/no/nav/syfo/config/caching/KeyGenerator.java
+++ provider/src/main/java/no/nav/syfo/config/caching/KeyGenerator.java
@@ -1,14 +1,11 @@
 package no.nav.syfo.config.caching;
 
 
-import no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 import static java.lang.Integer.toHexString;
-import static java.lang.reflect.Proxy.getInvocationHandler;
 import static java.lang.reflect.Proxy.isProxyClass;
 import static org.springframework.aop.framework.AopProxyUtils.proxiedUserInterfaces;
 
@@ -26,12 +23,7 @@ public class KeyGenerator extends SimpleKeyGenerator {
 
     private String getTargetClassName(Object target) {
         if (isProxyClass(target.getClass())) {
-            InvocationHandler invocationHandler = getInvocationHandler(target);
-            if (invocationHandler instanceof InstanceSwitcher) {
-                return ((InstanceSwitcher) invocationHandler).getTargetClassName();
-            } else {
-                return proxiedUserInterfaces(target)[0].getName();
-            }
+            return proxiedUserInterfaces(target)[0].getName();
         }
         return target.getClass().getName();
     }
