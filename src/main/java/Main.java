ed41627e455d71064b5965b009875676153635c1
Andreas Lund <andreas.lund@nav.no>
2018-03-16 13:35:30 +0100

    Setter manglende properties slik at metrics fungerer.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import no.nav.syfo.config.ApplicationConfig;
 
+import java.net.InetAddress;
+
 import static java.lang.System.*;
 import static no.nav.apiapp.ApiApp.startApp;
 
@@ -7,6 +9,9 @@ public class Main {
     public static void main(String... args) throws Exception {
         getenv().forEach(System::setProperty);
         setProperty("OIDC_REDIRECT_URL", getProperty("VEILARBLOGIN_REDIRECT_URL_URL"));
+        setProperty("applicationName", "fastlegerest");
+        setProperty("node.hostname", InetAddress.getLocalHost().getHostName());
+        setProperty("environment.name", getProperty("FASIT_ENVIRONMENT_NAME"));
         startApp(ApplicationConfig.class, args);
     }
 }
