6dd4ffade3a543f67f797dc9917c5d2123f4182f
June Henriksen <june.henriksen2@nav.no>
2019-01-17 11:07:11 +0100

    SYFOUTV-2612: Konfigurer opp endepunkt PartnerEmottak

--- src/main/java/no/nav/syfo/config/PartnerEmottakConfig.java
+++ src/main/java/no/nav/syfo/config/PartnerEmottakConfig.java
@@ -1,32 +1,32 @@
 package no.nav.syfo.config;
 
 import no.nav.emottak.schemas.PartnerResource;
-import no.nav.sbl.dialogarena.common.cxf.CXFClient;
-import no.nav.syfo.mocks.PartnerResourceMock;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import no.nav.syfo.consumer.util.ws.LogErrorHandler;
+import no.nav.syfo.consumer.util.ws.WsClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.*;
 
-import static java.lang.System.getProperty;
-import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
+import static java.util.Collections.singletonList;
+import static no.nav.syfo.consumer.util.ws.STSClientConfig.configureRequestSamlToken;
 
 @Configuration
 public class PartnerEmottakConfig {
 
-    private static final String MOCK_KEY = "partner.emottak.withmock";
-    private static final String ENDEPUNKT_URL = getProperty("PARTNER_WS_ENDPOINTURL");
-    private static final String ENDEPUNKT_NAVN = "PARTNER_EMOTTAK";
-
     @Bean
-    public PartnerResource partnerResource() {
-        PartnerResource mock = new PartnerResourceMock();
-        CXFClient<PartnerResource> factory = factory();
-        factory.factoryBean.setUsername(getProperty("SRVFASTLEGEREST_USERNAME"));
-        factory.factoryBean.setPassword(getProperty("SRVFASTLEGEREST_PASSWORD"));
-        PartnerResource prod = factory.build();
-        return createMetricsProxyWithInstanceSwitcher(ENDEPUNKT_NAVN, prod, mock, MOCK_KEY, PartnerResource.class);
+    @Primary
+    @ConditionalOnProperty(value="mockPartnerEmottak", havingValue = "false", matchIfMissing = true)
+    public PartnerResource partnerResource(@Value("${srvfastlegerest.username}") String username,
+                                           @Value("${srvfastlegerest.password}") String password,
+                                           @Value("${partner.ws.endpointurl}") String serviceUrl ){
+
+        PartnerResource port = new WsClient<PartnerResource>().createPort(
+                serviceUrl,
+                PartnerResource.class,
+                singletonList(new LogErrorHandler()));
+        configureRequestSamlToken(port);
+        return port;
+
     }
 
-    private CXFClient<PartnerResource> factory() {
-        return new CXFClient<>(PartnerResource.class).address(ENDEPUNKT_URL);
-    }
 }
