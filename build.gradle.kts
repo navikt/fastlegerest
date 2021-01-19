3bf5064498cf335ee502c1c32ad6e7988aca2a52
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-19 20:12:46 +0100

    Bump plugin com.github.johnrengelman.shadow
    Bump plugin com.github.johnrengelman.shadow version 4.0.3 -> 6.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     id("java")
     kotlin("jvm") version "1.4.21"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.4.21"
-    id("com.github.johnrengelman.shadow") version "4.0.3"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     id("org.springframework.boot") version "2.3.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
 }
