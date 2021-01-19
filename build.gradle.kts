e5a7f2d3db3d03c226225ecf3c2a31368fbe425b
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-19 20:33:53 +0100

    Bump Cxf version
    Bump Cxf version 3.3.6 -> 3.3.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ version = "1.0.0"
 
 val adal4jVersion = "1.6.4"
 val apacheHttpClientVersion = "4.5.6"
-val cxfVersion = "3.3.6"
+val cxfVersion = "3.3.7"
 val javaxWsRsApiVersion = "2.0.1"
 val kotlinJacksonVersion = "2.11.3"
 val navOidcVersion = "0.2.18"
