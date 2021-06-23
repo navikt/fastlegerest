589c1b3211248eef193af2c32ae0f66f2dd72d61
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-06-23 13:34:38 +0200

    Use azureAD config from azureator config for API V2

--- init.sh
+++ init.sh
@@ -2,7 +2,5 @@
 export CLIENT_ID=$(cat /secrets/azuread/fastlegerest/client_id)
 export CLIENT_SECRET=$(cat /secrets/azuread/fastlegerest/client_secret)
 
-export NO_NAV_SECURITY_JWT_ISSUER_VEILEDERAAD_ACCEPTEDAUDIENCE=$(cat /secrets/azuread/fastlegerest/client_id)
-
 export SRV_USERNAME=$(cat /secrets/serviceuser/fastlegerest/username)
 export SRV_PASSWORD=$(cat /secrets/serviceuser/fastlegerest/password)
