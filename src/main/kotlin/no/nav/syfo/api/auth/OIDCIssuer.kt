3d92ad436a164813450799eb1c9b3413d2106d5f
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-08-26 12:49:51 +0200

    Remove version 1 of Modia-api that is longer in use

--- src/main/kotlin/no/nav/syfo/api/auth/OIDCIssuer.kt
+++ src/main/kotlin/no/nav/syfo/api/auth/OIDCIssuer.kt
@@ -2,7 +2,6 @@ package no.nav.syfo.api.auth
 
 object OIDCIssuer {
     const val EKSTERN = "selvbetjening"
-    const val AZURE = "veileder"
     const val VEILEDER_AZURE_V2 = "veilederaad"
     const val STS = "sts"
 }
