c3b211ab011b7a046e97266273cea4b7b17af011
Anders Rognstad <anders.rognstad@nav.no>
2021-09-24 15:34:01 +0200

    expose foreldreEnhetHerId in Fastlege

--- src/test/kotlin/no/nav/syfo/fastlege/FastlegeRessursV2Test.kt
+++ src/test/kotlin/no/nav/syfo/fastlege/FastlegeRessursV2Test.kt
@@ -5,9 +5,10 @@ import no.nav.syfo.LocalApplication
 import no.nav.syfo.consumer.pdl.PdlConsumer
 import no.nav.syfo.consumer.tilgangskontroll.Tilgang
 import no.nav.syfo.consumer.tilgangskontroll.TilgangkontrollConsumer
+import no.nhn.register.communicationparty.ICommunicationPartyService
+import no.nhn.register.communicationparty.WSOrganizationPerson
 import no.nhn.schemas.reg.flr.IFlrReadOperations
 import org.hamcrest.Matchers
-import org.json.JSONException
 import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
@@ -47,6 +48,9 @@ class FastlegeRessursV2Test {
     @MockBean
     private lateinit var fastlegeSoapClient: IFlrReadOperations
 
+    @MockBean
+    private lateinit var adresseregisterSoapClient: ICommunicationPartyService
+
     @Before
     fun setUp() {
         Mockito.`when`(pdlConsumer.person(ArgumentMatchers.anyString()))
@@ -54,8 +58,8 @@ class FastlegeRessursV2Test {
     }
 
     @Test
-    @Throws(Exception::class)
     fun finnAktivFastlege() {
+        mockOrganisationPerson()
         mockHarFastlege(fastlegeSoapClient)
         Mockito.`when`(tilgangkontrollConsumer.accessAzureAdV2(FNR)).thenReturn(
             Tilgang(
@@ -63,16 +67,38 @@ class FastlegeRessursV2Test {
                 begrunnelse = ""
             )
         )
-        mvc.perform(MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer $token"))
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        )
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString(LEGEKONTOR)))
     }
 
     @Test
-    @Throws(Exception::class)
+    fun finnAktivFastlegeMedParentHerId() {
+        mockOrganisationPerson(PARENT_HER_ID)
+        mockHarFastlege(fastlegeSoapClient)
+        Mockito.`when`(tilgangkontrollConsumer.accessAzureAdV2(FNR)).thenReturn(
+            Tilgang(
+                harTilgang = true,
+                begrunnelse = ""
+            )
+        )
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString(PARENT_HER_ID.toString())))
+
+    }
+
+    @Test
     fun finnAlleFastleger() {
+        mockOrganisationPerson()
         mockHarFastlege(fastlegeSoapClient)
         Mockito.`when`(tilgangkontrollConsumer.accessAzureAdV2(FNR)).thenReturn(
             Tilgang(
@@ -80,15 +106,16 @@ class FastlegeRessursV2Test {
                 begrunnelse = ""
             )
         )
-        mvc.perform(MockMvcRequestBuilders.get("/api/v2/fastlege/fastleger?fnr=$FNR")
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer $token"))
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/fastlege/fastleger?fnr=$FNR")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        )
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString(LEGEKONTOR)))
     }
 
     @Test
-    @Throws(Exception::class)
     fun brukerHarIngenFastleger() {
         mockIngenFastleger(fastlegeSoapClient)
         Mockito.`when`(tilgangkontrollConsumer.accessAzureAdV2(FNR)).thenReturn(
@@ -97,15 +124,16 @@ class FastlegeRessursV2Test {
                 begrunnelse = ""
             )
         )
-        mvc.perform(MockMvcRequestBuilders.get("/api/v2/fastlege/fastleger?fnr=$FNR")
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer $token"))
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/fastlege/fastleger?fnr=$FNR")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        )
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.content().string("[]"))
     }
 
     @Test
-    @Throws(Exception::class)
     fun brukerManglerAktivFastlege() {
         mockIngenFastleger(fastlegeSoapClient)
         Mockito.`when`(tilgangkontrollConsumer.accessAzureAdV2(FNR)).thenReturn(
@@ -114,15 +142,16 @@ class FastlegeRessursV2Test {
                 begrunnelse = ""
             )
         )
-        mvc.perform(MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer $token"))
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        )
             .andExpect(MockMvcResultMatchers.status().isNotFound)
             .andExpect(MockMvcResultMatchers.content().json(mockApiError404()))
     }
 
     @Test
-    @Throws(Exception::class)
     fun brukerHarIkkeTilgang() {
         mockHarFastlege(fastlegeSoapClient)
         Mockito.`when`(tilgangkontrollConsumer.accessAzureAdV2(FNR)).thenReturn(
@@ -131,24 +160,23 @@ class FastlegeRessursV2Test {
                 begrunnelse = "GEOGRAFISK"
             )
         )
-        mvc.perform(MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer $token"))
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/fastlege?fnr=$FNR")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        )
             .andExpect(MockMvcResultMatchers.status().isForbidden)
             .andExpect(MockMvcResultMatchers.content().json(mockApiError403()))
     }
 
-    @Throws(JSONException::class)
     private fun mockApiError403(): String {
         return mockApiErrorAsJson(403, "GEOGRAFISK")
     }
 
-    @Throws(JSONException::class)
     private fun mockApiError404(): String {
         return mockApiErrorAsJson(404, "Feil ved oppslag av fastlege")
     }
 
-    @Throws(JSONException::class)
     private fun mockApiErrorAsJson(status: Int, message: String): String {
         val jsonObject = JSONObject()
         jsonObject.put("status", status)
@@ -156,10 +184,17 @@ class FastlegeRessursV2Test {
         return jsonObject.toString()
     }
 
+    private fun mockOrganisationPerson(parentHerId: Int? = null) {
+        val wsOrganisationPerson = WSOrganizationPerson().withParentHerId(parentHerId)
+        Mockito.`when`(adresseregisterSoapClient.getOrganizationPersonDetails(ArgumentMatchers.anyInt()))
+            .thenReturn(wsOrganisationPerson)
+    }
+
     companion object {
         private const val FNR = "99999900000"
         private const val VEILEDER_ID = "veilederID"
         private const val LEGEKONTOR = "Pontypandy Legekontor"
+        private const val PARENT_HER_ID: Int = 707
         private val token = JwtTokenGenerator.createSignedJWT(VEILEDER_ID).serialize()
     }
 }
