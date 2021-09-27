33ce194cbce100a0f498468dc71163771ce35990
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-09-27 15:36:29 +0200

    Support nav-personident in header

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -7,7 +7,7 @@ const val NAV_CONSUMER_TOKEN_HEADER = "Nav-Consumer-Token"
 const val NAV_CONSUMER_ID_HEADER = "Nav-Consumer-Id"
 const val APP_CONSUMER_ID = "syfoperson"
 
-const val NAV_PERSONIDENT_HEADER = "Nav-Personident"
+const val NAV_PERSONIDENT_HEADER = "nav-personident"
 
 const val TEMA_HEADER = "Tema"
 const val ALLE_TEMA_HEADERVERDI = "GEN"
