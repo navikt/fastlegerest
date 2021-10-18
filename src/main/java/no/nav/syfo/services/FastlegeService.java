7fb5e72a173aa153f7816f13df308b81aa4a191c
geir-waagboe <geir.arne.waagbo@nav.no>
2021-10-18 09:25:35 +0200

    Use isproxy instead of soap-api

--- src/main/java/no/nav/syfo/services/FastlegeService.java
+++ src/main/java/no/nav/syfo/services/FastlegeService.java
@@ -1,18 +1,14 @@
 package no.nav.syfo.services;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.syfo.consumer.fastlege.FastlegeConsumer;
+import no.nav.syfo.consumer.fastlege.PraksisInfo;
 import no.nav.syfo.consumer.pdl.*;
-import no.nav.syfo.consumer.ws.adresseregister.AdresseregisterConsumer;
-import no.nav.syfo.consumer.ws.adresseregister.OrganisasjonPerson;
 import no.nav.syfo.domain.*;
-import no.nav.syfo.services.exceptions.FastlegeIkkeFunnet;
-import no.nhn.schemas.reg.common.en.WSPeriod;
-import no.nhn.schemas.reg.flr.*;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -20,27 +16,20 @@ import java.util.function.Predicate;
 import static java.time.LocalDate.now;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
-import static no.nav.syfo.mappers.FastlegeMappers.ws2fastlege;
-import static no.nav.syfo.mappers.FastlegeMappers.ws2fastlegekontor;
-import static no.nav.syfo.util.MapUtil.map;
-import static no.nav.syfo.util.MapUtil.mapListe;
 import static no.nav.syfo.util.StringUtil.lowerCapitalize;
 
 @Slf4j
 @Service
 public class FastlegeService {
-    private final IFlrReadOperations fastlegeSoapClient;
     private final PdlConsumer pdlConsumer;
-    private final AdresseregisterConsumer adresseregisterConsumer;
+    private final FastlegeConsumer fastlegeConsumer;
 
     @Inject
     public FastlegeService(
-            final IFlrReadOperations fastlegeSoapClient,
             final PdlConsumer pdlConsumer,
-            final AdresseregisterConsumer adresseregisterConsumer) {
-        this.fastlegeSoapClient = fastlegeSoapClient;
+            final FastlegeConsumer fastlegeConsumer) {
         this.pdlConsumer = pdlConsumer;
-        this.adresseregisterConsumer = adresseregisterConsumer;
+        this.fastlegeConsumer = fastlegeConsumer;
     }
 
     @Cacheable(value = "fastlege")
@@ -55,32 +44,20 @@ public class FastlegeService {
         try {
             Optional<PdlHentPerson> maybePerson = Optional.ofNullable(pdlConsumer.person(brukersFnr));
             Pasient pasient = toPasient(maybePerson);
-            WSPatientToGPContractAssociation patientGPDetails = fastlegeSoapClient.getPatientGPDetails(brukersFnr);
-            return hentFastleger(patientGPDetails).stream()
+            List<Fastlege> fastlegeListe = fastlegeConsumer.getFastleger(brukersFnr);
+            return fastlegeListe.stream()
                     .map(fastlege -> fastlege
                             .pasient(new Pasient()
                                     .fnr(brukersFnr)
                                     .fornavn(pasient.fornavn())
                                     .mellomnavn(pasient.mellomnavn())
                                     .etternavn(pasient.etternavn()))
-                            .fastlegekontor(map(patientGPDetails.getGPContract().getGPOffice(), ws2fastlegekontor))
                             .foreldreEnhetHerId(hentForeldreEnhetHerId(fastlege).orElse(null))
                     ).collect(toList());
-        } catch (IFlrReadOperationsGetPatientGPDetailsGenericFaultFaultFaultMessage e) {
-            log.warn("Søkte opp og fikk en feil fra fastlegetjenesten. Dette skjer trolig fordi FNRet ikke finnes", e);
-            throw new FastlegeIkkeFunnet("Feil ved oppslag av fastlege");
         } catch (RuntimeException e) {
             log.error("Søkte opp og fikk en feil fra fastlegetjenesten fordi tjenesten er nede", e);
             throw e;
         }
-    }
-
-    private List<Fastlege> hentFastleger(WSPatientToGPContractAssociation patientGPDetails) {
-        return mapListe(patientGPDetails.getDoctorCycles().getGPOnContractAssociations(), ws2fastlege).stream()
-                .map(fastlege -> fastlege
-                        .pasientforhold(getPasientForhold(patientGPDetails.getPeriod()))
-                        .herId(patientGPDetails.getGPHerId()))
-                .collect(toList());
     }
 
     private Pasient toPasient(Optional<PdlHentPerson> maybePerson) {
@@ -102,20 +79,10 @@ public class FastlegeService {
         return pasient;
     }
 
-    private Pasientforhold getPasientForhold(WSPeriod period) {
-        return new Pasientforhold()
-                .fom(period.getFrom().toLocalDate())
-                .tom(period.getTo() == null ? LocalDate.parse("9999-12-31") : period.getTo().toLocalDate());
-    }
-
     private Optional<Integer> hentForeldreEnhetHerId(Fastlege fastlege) {
-        Optional<Integer> fastlegeForeldreEnhetHerId = ofNullable(fastlege.herId())
-                .map(adresseregisterConsumer::hentFastlegeOrganisasjonPerson)
-                .map(OrganisasjonPerson::getForeldreEnhetHerId);
-
-        log.info("DIALOGMELDING-TRACE: Fant fastlegeForeldreEnhetHerId: {}", fastlegeForeldreEnhetHerId.orElse(null));
-
-        return fastlegeForeldreEnhetHerId;
+        return ofNullable(fastlege.herId())
+                .map(fastlegeConsumer::getPraksisInfo)
+                .map(PraksisInfo::getForeldreEnhetHerId);
     }
 
     private static Predicate<Fastlege> isAktiv() {
--- src/main/kotlin/no/nav/syfo/consumer/ws/adresseregister/OrganisasjonPerson.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.syfo.consumer.ws.adresseregister
-
-data class OrganisasjonPerson(
-    val foreldreEnhetHerId: Int? = null
-)
